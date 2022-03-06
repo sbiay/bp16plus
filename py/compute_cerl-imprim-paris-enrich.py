aeeeeede681574dead899838facbf310fd7d11c3
Sebastien Biay <sbiayha@outlook.fr>
2022-03-06 16:47:08 +0100

    Gestion des problèmes de décodage de Json

--- py/compute_cerl-imprim-paris-enrich.py
+++ py/compute_cerl-imprim-paris-enrich.py
@@ -1,6 +1,7 @@
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import requests
+import json
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
@@ -23,8 +24,8 @@ selection = {
     'adresseParis': [],
 }
 
-# On écrira les id traités dans un fichier de rapport
-with open("/home/sbiay/chantiers/bp16plus/rapports/compute_cerl-imprim-paris-enrich.txt", mode="w") as f:
+# On ouvre un fichier de rapport d'erreur pour les problèmes de lecture de Json
+with open("/home/sbiay/chantiers/bp16plus/rapports/compute_cerl-imprim-paris-enrich-Json.txt", mode="w") as rapportJson:
 
     # On parse les données du dataset sous la forme d'une liste de dictionnaires
     for my_row_as_dict in cerl_imprim_paris.iter_rows():
@@ -33,44 +34,43 @@ with open("/home/sbiay/chantiers/bp16plus/rapports/compute_cerl-imprim-paris-enr
 
         # A partir de l'identifiant de la personne, on récupère sa notice complète au format Json
         r = requests.get(f"https://data.cerl.org/thesaurus/{my_row_as_dict['id']}?format=json")
-        noticeJson = r.json()
 
-        # On parse le contenu de ce Json
-        # On récupère les dates de la personne sous leur forme française
-        if noticeJson["data"].get("bioDates"):
-            for dates in noticeJson["data"].get("bioDates"):
-                # Pour s'assurer de ne pas ajouter deux dates, on pose comme condition que l'index courant
-                # n'a pas encore reçu de date
-                if len(selection["dates"]) < i+1:
-                    if dates["lang"] == "fre":
-                        selection["dates"].append(dates["text"])
+        # On récupère le Json dans un try afin d'éviter les problèmes de décodage de fichier
+        try:
+            noticeJson = r.json()
 
-        # On récupère le lien vers la notice DataBNF
-        if noticeJson["data"].get("extDataset"):
-            for liens in noticeJson["data"]["extDataset"]:
-                if len(selection["dataBNF"]) < i+1:
-                    if liens["searchTerm"][:18] == "http://data.bnf.fr":
-                        selection["dataBNF"].append(liens["searchTerm"])
+            # On parse le contenu de ce Json
+            # On récupère les dates de la personne sous leur forme française
+            if noticeJson["data"].get("bioDates"):
+                for dates in noticeJson["data"].get("bioDates"):
+                    # Pour s'assurer de ne pas ajouter deux dates, on pose comme condition que l'index courant
+                    # n'a pas encore reçu de date
+                    if len(selection["dates"]) < i+1:
+                        if dates["lang"] == "fre":
+                            selection["dates"].append(dates["text"])
+            # On récupère le lien vers la notice DataBNF
+            if noticeJson["data"].get("extDataset"):
+                for liens in noticeJson["data"]["extDataset"]:
+                    if len(selection["dataBNF"]) < i+1:
+                        if liens["searchTerm"][:18] == "http://data.bnf.fr":
+                            selection["dataBNF"].append(liens["searchTerm"])
+            # On récupère l'adresse de l'imprimeur
+            if noticeJson["data"].get("place"):
+                for lieu in noticeJson["data"]["place"]:
+                    if len(selection["adresseParis"]) < i+1:
+                        if lieu["part"][0]["name"] == "Paris" and lieu["part"][0].get("address"):
+                            if len(lieu["part"]) > 1:
+                                selection['adresseParis'].append(lieu["part"][1]["address"])
+            # TRES IMPORTANT : s'il n'y a pas de résultat dans le contenu parsé, on ajoute un NULL
+            # car la cohérence des données repose sur l'absence de vide
+            for cle in selection:
+                if len(selection[cle]) == i:
+                    selection[cle].append("NULL")
+            # On implémente l'index suivant
+            i += 1
 
-        # On récupère l'adresse de l'imprimeur
-        if noticeJson["data"].get("place"):
-            for lieu in noticeJson["data"]["place"]:
-                if len(selection["adresseParis"]) < i+1:
-                    if lieu["part"][0]["name"] == "Paris" and lieu["part"][0].get("address"):
-                        if len(lieu["part"]) > 1:
-                            selection['adresseParis'].append(lieu["part"][1]["address"])
-
-        # TRES IMPORTANT : s'il n'y a pas de résultat dans le contenu parsé, on ajoute un NULL
-        # car la cohérence des données repose sur l'absence de vide
-        for cle in selection:
-            if len(selection[cle]) == i:
-                selection[cle].append("NULL")
-
-        # On écrit le rapport
-        f.write(f"Index {i} traité avec succès - id : {my_row_as_dict['id']}\n")
-
-        # On implémente l'index suivant
-        i += 1
+        except json.decoder.JSONDecodeError:
+            rapportJson.write(f"There was a problem accessing the equipment data on {my_row_as_dict['id']}.")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 cerl_imprim_paris_enrich_df = pd.DataFrame(selection)
