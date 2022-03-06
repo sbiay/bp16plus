ab57f50bd500adfab94c31c5fcaa274726c3ca12
Sebastien Biay <sbiayha@outlook.fr>
2022-03-06 17:32:34 +0100

    Débuggage de la cohérence des données avec des conditions de longueur de résultats + reconstruction des URI DataBNF par les liens BNF CG

--- py/compute_cerl-imprim-paris-enrich.py
+++ py/compute_cerl-imprim-paris-enrich.py
@@ -6,16 +6,13 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
-# Read recipe inputs
+# On importe le jeu de données à traiter
 cerl_imprim_paris = dataiku.Dataset("cerl-imprim-paris_filtered")
-cerl_imprim_paris_df = cerl_imprim_paris.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# On initie l'index des lignes que l'on va traiter
 i = 0
-# On initie une liste pour les futurs résultats
+# On initie le dataFrame pour les futures données parsées
 selection = {
     'id': [],
     'nom': [],
@@ -29,8 +26,18 @@ with open("/home/sbiay/chantiers/bp16plus/rapports/compute_cerl-imprim-paris-enr
 
     # On parse les données du dataset sous la forme d'une liste de dictionnaires
     for my_row_as_dict in cerl_imprim_paris.iter_rows():
-        selection["id"].append(my_row_as_dict["id"])
-        selection["nom"].append(my_row_as_dict["name_display_line"])
+        if i == 200:
+            break
+        
+        # On récupère l'id
+        # Pour s'assurer de ne pas ajouter deux dates, on pose comme condition que l'index courant
+        # n'a pas encore reçu de date
+        if len(selection["id"]) < i+1:
+            selection["id"].append(my_row_as_dict["id"])
+
+        # On récupère le nom de la personne
+        if len(selection["nom"]) < i+1:
+            selection["nom"].append(my_row_as_dict["name_display_line"])
 
         # A partir de l'identifiant de la personne, on récupère sa notice complète au format Json
         r = requests.get(f"https://data.cerl.org/thesaurus/{my_row_as_dict['id']}?format=json")
@@ -40,27 +47,35 @@ with open("/home/sbiay/chantiers/bp16plus/rapports/compute_cerl-imprim-paris-enr
             noticeJson = r.json()
 
             # On parse le contenu de ce Json
-            # On récupère les dates de la personne sous leur forme française
-            if noticeJson["data"].get("bioDates"):
-                for dates in noticeJson["data"].get("bioDates"):
-                    # Pour s'assurer de ne pas ajouter deux dates, on pose comme condition que l'index courant
-                    # n'a pas encore reçu de date
-                    if len(selection["dates"]) < i+1:
+            # On récupère les dates au format français
+            if len(selection["dates"]) < i+1:
+                if noticeJson["data"].get("bioDates"):
+                    for dates in noticeJson["data"]["bioDates"]:
+            
                         if dates["lang"] == "fre":
                             selection["dates"].append(dates["text"])
             # On récupère le lien vers la notice DataBNF
-            if noticeJson["data"].get("extDataset"):
-                for liens in noticeJson["data"]["extDataset"]:
-                    if len(selection["dataBNF"]) < i+1:
+            if len(selection["dataBNF"]) < i+1:
+                if noticeJson["data"].get("extDataset"):
+                    for liens in noticeJson["data"]["extDataset"]:
                         if liens["searchTerm"][:18] == "http://data.bnf.fr":
-                            selection["dataBNF"].append(liens["searchTerm"])
+                            selection["dataBNF"].append(liens["searchTerm"] + "#about")
+            # Faute d'un identifiant dataBNF, on le reconstruit en récupérant l'ark
+            # s'il existe un lien vers une notice du CG BNF
+            if len(selection["dataBNF"]) < i+1:
+                if noticeJson["data"].get("extDataset"):
+                    for liens in noticeJson["data"]["extDataset"]:
+                        if liens["searchTerm"][:18] == "http://catalogue.bn":
+                            selection["dataBNF"].append("http://data.bnf.fr" + liens["searchTerm"][23:] + "#about")
+                        
             # On récupère l'adresse de l'imprimeur
-            if noticeJson["data"].get("place"):
-                for lieu in noticeJson["data"]["place"]:
-                    if len(selection["adresseParis"]) < i+1:
-                        if lieu["part"][0]["name"] == "Paris" and lieu["part"][0].get("address"):
+            if len(selection["adresseParis"]) < i+1:
+                if noticeJson["data"].get("place"):
+                    for lieu in noticeJson["data"]["place"]:
+                        if lieu["part"][0]["name"] == "Paris":
                             if len(lieu["part"]) > 1:
-                                selection['adresseParis'].append(lieu["part"][1]["address"])
+                                if lieu["part"][1].get("address"):
+                                    selection['adresseParis'].append(lieu["part"][1]["address"])
             # TRES IMPORTANT : s'il n'y a pas de résultat dans le contenu parsé, on ajoute un NULL
             # car la cohérence des données repose sur l'absence de vide
             for cle in selection:
@@ -71,6 +86,11 @@ with open("/home/sbiay/chantiers/bp16plus/rapports/compute_cerl-imprim-paris-enr
 
         except json.decoder.JSONDecodeError:
             rapportJson.write(f"There was a problem accessing the equipment data on {my_row_as_dict['id']}.")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Vérification de la cohérence des résultats
+for cle in selection:
+    print(cle + ": " + str(len(selection[cle])) + " - " + str(selection[cle][199]))
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 cerl_imprim_paris_enrich_df = pd.DataFrame(selection)
