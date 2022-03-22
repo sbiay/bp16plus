73ec0b3e0124ba2ed8b306edaeb6746f0cf81a30
Sebastien Biay <sbiayha@outlook.fr>
2022-03-22 16:40:02 +0100

    Nettoyage pour pull request

--- afaire.md
+++
@@ -1,4 +0,0 @@
-BP16 plus : à faire
-====
-
-- Noms de rues : ajouter à la main les coordonnées géographiques du Palais
--- py/requetes.py
+++
@@ -1,8 +0,0 @@
-with open("requetes/cerl-imprim-paris.text", mode="w") as f:
-    r = "https://data.cerl.org/thesaurus/_search?query=(" \
-        "imprimeur%20paris)%20AND%20type%3Acnp&size=100&mode=default&format=json&from="
-    hits = 4557
-    compteur = 1
-    while compteur < hits:
-        f.write(r + str(compteur) + "\n")
-        compteur += 100
